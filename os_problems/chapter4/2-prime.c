80c97ede52a6d3baa6a35e6f2f2bad80768cf498
Chaebum Kim <tw2cha@gmail.com>
2021-06-14 14:01:48 +0900

    Problem 4.23

--- os_problems/chapter4/2-prime.c
+++ os_problems/chapter4/2-prime.c
@@ -1,5 +1,5 @@
 /* 2-prime.c
-** A multithreaded program that outputs prime numbers.
+*  A multithreaded program that outputs prime numbers.
 */
 
 #include <pthread.h>
@@ -7,12 +7,8 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// Global variables
-int *prime_nums;
-int count = 0;
-
 // Worker function
-void *get_primes(void *num);
+void *output_primes(void *num);
 
 // Helper function
 _Bool is_prime(int num);
@@ -21,50 +17,30 @@ int main(int argc, char *argv[])
 {
     int num;
     pthread_t tid;
-    pthread_attr_t attr;
 
     // Get user input
     printf("Number: ");
     scanf("%d", &num);
 
-    // Set the default attributes of the thread
-    pthread_attr_init(&attr);
-
     // Create the thread
-    pthread_create(&tid, &attr, get_primes, &num);
+    pthread_create(&tid, NULL, output_primes, &num);
 
     // Wait for the thread to exit
     pthread_join(tid, NULL);
 
-    // Print the result
-    for (int i = 0; i < count; i++)
-    {
-        printf("%d ", prime_nums[i]);
-    }
-    printf("\n");
-
-    free(prime_nums);
-
     return 0;
 }
 
-void *get_primes(void *num)
+void *output_primes(void *num)
 {
-    int j = 0;
     int num_ = *(int *)num;
-
-    // Allocate memory
-    prime_nums = (int *)malloc(sizeof(int) * num_);
 
     for (int i = 1; i <= num_; i++)
     {
         if (is_prime(i))
-        {
-            prime_nums[j] = i;
-            j++;
-            count++;
-        }
+            printf("%d ", i);
     }
+    printf("\n");
 
     pthread_exit(0);
 }
